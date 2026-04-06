608fd05716013675863060fc5143a4872af97430
Erika Jen Tugas <erikajentugas@Erikas-MacBook-Pro.local>
2026-04-05 18:43:56 -0700

    Fix TutoringApiApplication

--- src/main/java/com/tutoring/tutoring_api/TutoringApiApplication.java
+++ src/main/java/com/tutoring/tutoring_api/TutoringApiApplication.java
@@ -1,17 +1,27 @@
-public static void main(String[] args) {
-	boolean isRailway = System.getenv("RAILWAY_ENVIRONMENT") != null;
+package com.tutoring.tutoring_api;
 
-	if (isRailway) {
-		System.setProperty("spring.datasource.url", "jdbc:postgresql://hopper.proxy.rlwy.net:50797/railway");
-		System.setProperty("spring.datasource.username", "postgres");
-		System.setProperty("spring.datasource.password", "eTeGKacXhppCeFfvOKRJaYnLbfmYWOir");
-	} else {
-		System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5433/tutoring_db");
-		System.setProperty("spring.datasource.username", "postgres");
-		System.setProperty("spring.datasource.password", "0527");
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class TutoringApiApplication {
+
+	public static void main(String[] args) {
+		boolean isRailway = System.getenv("RAILWAY_ENVIRONMENT") != null;
+
+		if (isRailway) {
+			System.setProperty("spring.datasource.url", "jdbc:postgresql://hopper.proxy.rlwy.net:50797/railway");
+			System.setProperty("spring.datasource.username", "postgres");
+			System.setProperty("spring.datasource.password", "eTeGKacXhppCeFfvOKRJaYnLbfmYWOir");
+		} else {
+			System.setProperty("spring.datasource.url", "jdbc:postgresql://localhost:5433/tutoring_db");
+			System.setProperty("spring.datasource.username", "postgres");
+			System.setProperty("spring.datasource.password", "0527");
+		}
+
+		System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
+		System.setProperty("spring.sql.init.mode", "never");
+		SpringApplication.run(TutoringApiApplication.class, args);
 	}
-
-	System.setProperty("spring.datasource.driver-class-name", "org.postgresql.Driver");
-	System.setProperty("spring.sql.init.mode", "never");
-	SpringApplication.run(TutoringApiApplication.class, args);
 }
